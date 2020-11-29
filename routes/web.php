e7d0c76d6e0fd075ccc9af072389c52529ab4537
Programmer <abbassmortazavi@gmail.com>
2020-11-29 19:57:33 +0330

    food and menu create completly

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::resource('food', App\Http\Controllers\FoodController::class);
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+
+Route::resource('menus', App\Http\Controllers\MenuController::class);
